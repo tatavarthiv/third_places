11ab1c5efccdfb0806d78b33b4312da57ec569b9
Andrew Zhang <andrewzhang09@yahoo.com>
2024-10-03 16:09:18 -0700

    integrate firebase with flutter via cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:parks/screens/tabs.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // initialize Firebase within Flutter app
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const MaterialApp(
       home: TabsScreen(),
