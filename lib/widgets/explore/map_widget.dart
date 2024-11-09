a512eb028452c3ac606aee70f34f810631e86a0f
janetio <tioj@uchicago.edu>
2024-11-09 13:51:28 -0500

    making height shorter

--- lib/widgets/explore/map_widget.dart
+++ lib/widgets/explore/map_widget.dart
@@ -55,7 +55,7 @@ class ParkMapWidget extends ConsumerWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Container(
-        height: 300, 
+        height: 275, 
         width: 375,
         child: Stack(
           children: [
